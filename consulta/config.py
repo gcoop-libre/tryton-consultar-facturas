4711ef59f6844508ab24647ba04b05fc90aacb2f
Luciano Rossi <lukio@gcoop.coop>
2020-04-03 10:39:01 -0300

    set SECRET_KEY with random uuid

--- consulta/config.py
+++ consulta/config.py
@@ -1,16 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-#from flask_compress import Compress
 import os
 import logging
+import uuid
 from logging.handlers import RotatingFileHandler
 
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = '1d94e52c-1c89-4515-b87a-f48cf3cb7f0b'
-    #LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    SECRET_KEY = uuid.uuid4().hex
     LOGGING_FORMAT = "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
     LOGGING_LOCATION = 'consulta.log'
     LOGGING_LEVEL = logging.DEBUG
@@ -20,7 +19,7 @@ class BaseConfig(object):
     COMPRESS_MIN_SIZE = 500
     BOOTSTRAP_SERVE_LOCAL = True
     TRYTON_DATABASE = ''
-    TRYTOND_CONFIG = '/opt/trytond/trytond.conf'
+    TRYTON_CONFIG = ''
     TRYTON_USER = '1'
     CAPTCHA_ENABLE = True
     CAPTCHA_LENGTH = 5
@@ -30,14 +29,14 @@ class BaseConfig(object):
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = False
-    SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39f'
+    SECRET_KEY = uuid.uuid4().hex
     TRYTON_DATABASE = ''
 
 
 class TestingConfig(BaseConfig):
     DEBUG = False
     TESTING = True
-    SECRET_KEY = '792842bc-c4df-4de1-9177-d5207bd9faa6'
+    SECRET_KEY = uuid.uuid4().hex
 
 config = {
     "development": "consulta.config.DevelopmentConfig",
@@ -56,5 +55,3 @@ def configure_app(app):
     formatter = logging.Formatter(app.config['LOGGING_FORMAT'])
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
-    # Configure Compressing
-    #Compress(app)
